470ef077237c82c7ab7ba2def07241a6a1f9349c
Johann <johannkoenig@google.com>
2014-06-16 11:10:07 -0700

    Turn on libvpx x86 optimizations [DO NOT MERGE]
    With yasm available we can use the assembly:
    https://android-review.googlesource.com/#/c/96972/
    
    Comparing with the VP9 video playback with C-reference software decode,
    before and after these patches applied,
    
    Performance data with 2 test clips:
    Clips                                    bit rate    fps(after)  fps(before)
    vp9_720p_qp10to20_1510Kbps_30fps.webm	  1510Kbps      30	     12
    vp9_720p_qp20to30_840Kbps_30fps.webm	  840Kbps       30	     13
    
    (cherry-picked from afecaee6099284387688b9ce17ec3642ec4e5c1b)
    
    Change-Id: I9c67e5ae302f923134e073f4bafb09fb1f3fe94d

--- config.x86.mk
+++ config.x86.mk
@@ -3,7 +3,7 @@
 # libvpx_codec_srcs_c_x86
 # libvpx_codec_srcs_asm_x86
 
-libvpx_target := generic
+libvpx_target := x86
 
 libvpx_config_dir_x86 := $(LOCAL_PATH)/$(libvpx_target)
 libvpx_codec_srcs := $(sort $(shell cat $(libvpx_config_dir_x86)/libvpx_srcs.txt))
@@ -13,4 +13,5 @@ libvpx_codec_srcs_c_x86 := $(addprefix libvpx/, $(filter-out vpx_config.c, \
     $(filter %.c, $(libvpx_codec_srcs)))) \
     $(libvpx_target)/vpx_config.c
 
-libvpx_codec_srcs_asm_x86 := $(filter %.asm.s, $(libvpx_codec_srcs))
+# X86 asm files are processed by the system and sent to yasm
+libvpx_codec_srcs_c_x86 += $(addprefix libvpx/, $(filter %.asm, $(libvpx_codec_srcs)))
